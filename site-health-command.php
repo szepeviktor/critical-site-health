d1b710192092cf6f633396272366101dd4799720
Viktor Szépe <viktor@szepe.net>
2025-05-15 04:03:36 +0000

    Fix autoloader

--- site-health-command.php
+++ site-health-command.php
@@ -6,6 +6,8 @@ if (! class_exists('WP_CLI')) {
     return;
 }
 
-require_once __DIR__ . '/vendor/autoload.php';
+if (is_readable(__DIR__ . '/vendor/autoload.php')) {
+    include_once __DIR__ . '/vendor/autoload.php';
+}
 
 WP_CLI::add_command('site-health', 'SzepeViktor\\WP_CLI\\SiteHealth\\Command');
